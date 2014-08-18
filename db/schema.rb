bbe576de028e41c211fa7a30a9314b6f247d5428
Brandon Jones <brandon.jones9@gmail.com>
2014-08-17 22:38:41 -0700

    FIXED
    it all looks pretty no more errors everything is good fonts actually work right.

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717071633) do
+ActiveRecord::Schema.define(version: 20140817061455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "blogs", force: true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "guestbooks", force: true do |t|
     t.string   "name"
